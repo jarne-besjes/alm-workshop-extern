2b5de30ff5e1d8c35b337fa1b5c3956d6800b6ec
Jarne <91670760+jarne-besjes@users.noreply.github.com>
2025-12-01 13:21:34 +0000

    Update

--- workshop-service/main.go
+++ workshop-service/main.go
@@ -11,6 +11,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 		"message": "Welcome to the ALM Workshop!",
 	}
 
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
